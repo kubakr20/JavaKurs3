95cb3e6b6d0bf04308ea9d060183b0acadc23bd5
Jacob <kubakr20@gmail.com>
2018-08-19 15:13:42 +0200

    ok

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	
+        Random random = new Random();
+
+        System.out.println(random.nextInt(100));
     }
 }
